55519df2c10d2f302763b7eccb96036f8c510498
Jatin Rautela <curioussnake02@gmail.com>
2023-08-02 10:01:30 +0530

    added condition for separated Vnet module

--- vnet.tf
+++ vnet.tf
@@ -49,16 +49,6 @@ variable "vnet_name" {
   type        = string
 }
 
-variable "resource_group_name" {
-  description = "The name of the resource group to create the resources in."
-  type        = string
-}
-
-variable "location" {
-  description = "The location to create the resources in."
-  type        = string
-}
-
 variable "address_spaces" {
   description = "A list of address spaces that are used for this virtual network."
   type        = list(string)
@@ -109,12 +99,6 @@ variable "subnets" {
   }))
 
   default = {}
-}
-
-variable "tags" {
-  description = "A map of tags to assign to the resources."
-  type        = map(string)
-  default     = {}
 }
 
 output "vnet_id" {
